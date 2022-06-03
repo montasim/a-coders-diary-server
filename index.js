e6f470626596d69ec07886e129a9c880c90be692
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-06-03 19:45:37 +0600

    create user

--- index.js
+++ index.js
@@ -14,11 +14,21 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
+    const userCollection = client.db("a-coders-diary").collection("users");
     const postsCollection = client.db("a-coders-diary").collection("posts");
     const tagsCollection = client.db("a-coders-diary").collection("tags");
 
     try {
         await client.connect();
+
+        // create a user
+        app.post('/create-user', async (req, res) => {
+            const userData = req?.body;
+
+            const result = await userCollection.insertOne(userData);
+
+            res.send(result);
+        });
 
         // find all posts
         app.get('/posts', async (req, res) => {
@@ -35,10 +45,9 @@ async function run() {
             };
         });
 
-        // find a post
-        app.get('/posts/:_id', async (req, res) => {
+        // edit a post
+        app.get('/edit-post/:_id', async (req, res) => {
             const _id = req?.params?._id;
-            console.log(_id);
             const query = { _id: ObjectId(_id) };
             const post = await postsCollection.findOne(query);
 
@@ -78,12 +87,21 @@ async function run() {
             res.send(result);
         });
 
-        // find a tag
-        app.get('/tags/:_id', async (req, res) => {
+        // edit a tag
+        app.get('/edit-tag/:_id', async (req, res) => {
             const _id = req?.params?._id;
-            console.log(_id);
             const query = { _id: ObjectId(_id) };
             const tag = await tagsCollection.findOne(query);
+
+            res.send(tag);
+        });
+
+        // delete a tag
+        app.delete('/delete-tag/:_id', async (req, res) => {
+            const _id = req?.params?._id;
+            console.log(_id)
+            const query = { _id: ObjectId(_id) };
+            const tag = await tagsCollection.deleteOne(query);
 
             res.send(tag);
         });
